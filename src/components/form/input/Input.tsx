a3c63c7c864b1d1372e73225b3fe1caca3ca4bf3
Alex Symonds <alex-symonds@gmail.com>
2024-08-05 15:50:34 +0100

    Add: Input component

---
+++ src/components/form/input/Input.tsx
@@ -0,0 +1,42 @@
+
+
+import { mergeStyleArraysIntoString } from '@/components/utils';
+import styles from './Input.module.scss';
+import { ChangeEvent } from 'react';
+
+
+type T_PropsInput = {
+    handleChange: (e : ChangeEvent<HTMLInputElement>) => void,
+    hasError? : boolean,
+    id? : string,
+    stylesIn? : string[],
+    type?: "text" | "email" | "tel" | "date" | "time",
+    value: string,
+}
+
+
+export function Input({ handleChange, hasError, id, stylesIn, type, value } : T_PropsInput){
+
+    const CSS = mergeStyleArraysIntoString({
+        passedIn: stylesIn,
+        classesToAdd: [styles.input],
+        conditionalClasses: [ hasError ? styles.error : styles.default ]
+    });
+
+    const errorCSS = mergeStyleArraysIntoString({
+        classesToAdd: [styles.errorWrapper],
+        conditionalClasses: [hasError ? styles.errorWrapperOn : undefined]
+    })
+
+    return (
+    <div className = { errorCSS }>
+        <input 
+            className={ CSS }
+            id = { id }
+            type = { type ?? "text" }
+            value = { value }
+            onChange = { (e : ChangeEvent<HTMLInputElement>) => handleChange(e) }
+        />
+    </div>
+    )
+}
